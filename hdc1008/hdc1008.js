97fd0bfaccdbba03d6b6bc4dbef7915fab010831
pinacolada547 <pinacolada547@gmail.com>
2017-05-09 19:46:05 +0200

    hdc lib added

---
+++ hdc1008/hdc1008.js
@@ -0,0 +1,38 @@
+var i2c = require('i2c');
+var _ = require('lodash');
+var utils = require('./utils');
+
+var HDC1008 = function (opts) {
+    this.options = _.extend({}, {
+        address: 0x43,
+        device: '/dev/i2c-1',
+        command: 0x00,
+        length: 2
+    }, opts);
+    this.wire = new i2c(this.options.address, {device: this.options.device});
+};
+
+HDC1008.prototype.readTemperature = function (cb) {
+    var self = this;
+    if (!utils.exists(cb)) {
+        console.error("missing callback");
+        return;
+    }
+    this.wire.writeByte(this.options.command, function (err) {
+        if (utils.exists(err)) {
+            console.error("error write byte to HDC1008 - command: ", self.options.command);
+        }
+    });
+    this.wire.readBytes(this.options.command, this.options.length, function (err, res) {
+        var byte1 = res.readUInt8(0);
+        var byte2 = res.readUInt8(1);
+
+        var temp = (byte1 * 256) + byte2;
+        var celsTemp = (temp / 65536.0) * 165.0 - 40;
+
+
+        cb.call(self, celsTemp);
+    });
+};
+
+module.exports = HDC1008;

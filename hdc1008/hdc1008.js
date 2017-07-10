84bcf0799f10a01cff282928720b7a97e6ba230b
Sonja <sonja.schmickler@freenet.de>
2017-07-10 20:32:48 +0200

    zurück zum funktionierenden stand! Zunächst nur der temperatur wert! Immer Betrag des Sensorwertes nutzen? --> eigentlich nicht gut!

--- hdc1008/hdc1008.js
+++ hdc1008/hdc1008.js
@@ -18,6 +18,16 @@ HDC1008.prototype.readTemperature = function (cb) {
         console.error("missing callback");
         return;
     }
+
+
+    this.wire.writeByte(0x02, function (err) {
+
+        if (utils.exists(err)) {
+            console.error("error write byte to HDC1008 - command: ","0x02");
+
+        }
+    });
+
     this.wire.writeByte(this.options.command, function (err) {
         if (utils.exists(err)) {
             console.error("error write byte to HDC1008 - command: ", self.options.command);
@@ -28,7 +38,10 @@ HDC1008.prototype.readTemperature = function (cb) {
         var byte2 = res.readUInt8(1);
 
         var temp = (byte1 * 256) + byte2;
-        var celsTemp = (temp / 65536.0) * 165.0 - 40;
+        var celsTemp =((temp / 65536.0) * 165.0) - 40;
+
+        console.info("var celstemp: 2",celsTemp);
+  	    celsTemp = Math.abs(celsTemp);
 
 
         cb.call(self, celsTemp);
@@ -36,3 +49,4 @@ HDC1008.prototype.readTemperature = function (cb) {
 };
 
 module.exports = HDC1008;
+
